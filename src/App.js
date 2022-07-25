c344256d43c2cf0270991857f7afdaf46909bfef
Rachel Hill <hillrachel187@gmail.com>
2022-07-25 17:34:11 -0600

    Successfully fetch data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const App = () => {
+  const [articles, setArticles] = useState([]);
+
+  const fetchData = async () => {
+    const url = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=nhMjxLLY9q8nUWSK6HWsK65vMVRqNkUi'
+    try {
+      const response = await fetch(url)
+      const articles = await response.json()
+      setArticles(articles)
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 }
 
 export default App;
